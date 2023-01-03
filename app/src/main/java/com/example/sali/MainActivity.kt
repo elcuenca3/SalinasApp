7073d1c711d959c69aca315dfe452c694cdbe803
elcuenca3 <elcuenca3@utpl.edu.ec>
2023-01-03 18:13:14 -0500

    layouts terminados

--- app/src/main/java/com/example/sali/MainActivity.kt
+++ app/src/main/java/com/example/sali/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sali
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
