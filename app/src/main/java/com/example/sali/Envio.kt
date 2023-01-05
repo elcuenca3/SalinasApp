2d99fcb5a8269bdd22bc17b174059c165f79465e
mattseb <mateosmv1608@gmail.com>
2023-01-04 23:38:22 -0500

    cambios

--- app/src/main/java/com/example/sali/Envio.kt
+++ app/src/main/java/com/example/sali/Envio.kt
@@ -1,11 +1,97 @@
-package com.example.sali
-
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileInputStream
+import java.io.OutputStream
+import java.util.*
+import android.Manifest
+import com.example.sali.R
+
+const val REQUEST_BLUETOOTH_PERMISSION = 1
+const val REQUEST_ENABLE_BT = 2
 
 class Envio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_envio)
+
+        // Check if Bluetooth is supported on the device
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Device does not support Bluetooth", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Check if Bluetooth is not enabled, and ask the user to enable it
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
+        // Check if the app has the BLUETOOTH permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+            // Request the BLUETOOTH permission
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH), REQUEST_BLUETOOTH_PERMISSION)
+        } else {
+            // Permission has already been granted
+            sendFileOverBluetooth()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_BLUETOOTH_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission has been granted
+                    sendFileOverBluetooth()
+                } else {
+                    // Permission has been denied
+                }
+                return
+            }
+            else -> {
+                // Handle other permission requests
+            }
+        }
+    }
+
+    private fun sendFileOverBluetooth() {
+        val fileToSend = File("/path/to/file.txt")
+        val fileInputStream = FileInputStream(fileToSend)
+        val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        if (pairedDevices?.isNotEmpty() == true) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (device in pairedDevices) {
+                val deviceName = device.name
+                val deviceHardwareAddress = device.address // MAC address
+                if (deviceName == "My Bluetooth Device") {
+                    // This is the device we want to connect to
+                    val socket: BluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(uuid)
+                    socket.connect()
+                    val outputStream: OutputStream = socket.outputStream
+                    val buffer = ByteArray(4096)
+                    var bytesRead: Int
+                    while (fileInputStream.read(buffer).also { bytesRead = it } > 0) {
+                        outputStream.write(buffer, 0, bytesRead)
+                    }
+                    fileInputStream.close()
+                    outputStream.close()
+                    socket.close()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/sali/MainActivity.kt
+++ app/src/main/java/com/example/sali/MainActivity.kt
@@ -1,19 +1,49 @@
 package com.example.sali
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (existeArchivo((fileList()), "usuarios.txt")){
+            var contenido = ""
+            var archivo = InputStreamReader(openFileInput("usuarios.txt"))
+            var bf = BufferedReader(archivo)
+            var linea = bf.readLine()
+            while (linea != null){
+                contenido = contenido + linea + "\n"
+                linea = bf.readLine()
+            }
+            celularUser?.setText(contenido)
 
+        }
         registro.setOnClickListener{
             var saltar: Intent = Intent(this, Envio::class.java)
             startActivity(saltar)
-
         }
+    }
+    fun guardarArchivo(view: View){
+        var archivo = OutputStreamWriter(openFileOutput("usuarios.txt", Activity.MODE_PRIVATE))
+        archivo.write(celularUser.text.toString())
+        archivo.flush()
+        archivo.close()
+    }
+    fun existeArchivo(archivos: Array<String>, archivo:String): Boolean{
+        archivos.forEach {
+            if (archivo == it){
+                return true
+            }
+        }
+        return false
     }
 }
